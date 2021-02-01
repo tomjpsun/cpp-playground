bb0b2d16a9ce5b38ce70ca61a525a6f401c51057
Ching-Ping Sun <tomjpsun@gmail.com>
2021-02-01 10:52:11 +0800

    refactor: make output of armadillo_test to be more terse

--- src/armadillo_test.cpp
+++ src/armadillo_test.cpp
@@ -23,8 +23,6 @@ int armadillo_test()
 	cout << "max of col(3) is 20, on index 4" << endl;
 	cout << "max of col(4) is 25, on index 4" << endl << endl;
 
-	A.fill(0);
-
 	/*
 	  arma::find() 第一個參賽可以是 matrix 或 vec 或 relation (>, >=, <, <=, ==)
 	  如果是 relation 就表示找出符合 relation 的 indexes.
@@ -32,10 +30,11 @@ int armadillo_test()
 	  本例 find 是找出 B.col(i) 中, 等於最大值的 index, 只取一個.
 	  由於 armadillo 是從 0 開始計數, 標示 label 的 data 都是從 1 開始計數, 因此加一
 	 */
-	for (int i=0; i<B.n_cols; i++) {
-		A(i) = arma::as_scalar(arma::find( arma::max(B.col(i)) == B.col(i), 1)) + 1;
-	}
-	cout << A << endl;
+	rowvec q(B.n_cols, 0);
 
+	for (int i=0; i<B.n_cols; i++) {
+		q(i) =  arma::as_scalar(arma::find( arma::max(B.col(i)) == B.col(i), 1)) + 1;
+	}
+	cout << "q=" << q << endl;
 	return 0;
 }
