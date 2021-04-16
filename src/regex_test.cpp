2c41d360470519a72cc4469bf12d7771c47c6697
Ching-Ping Sun <tomjpsun@gmail.com>
2021-04-16 17:40:23 +0800

    add regex match

--- src/regex_test.cpp
+++ src/regex_test.cpp
@@ -35,7 +35,7 @@ void regex_test2()
 {
 	string target0("\x0a@2021/04/16 16:25:29.390-Antenna1-U\x0d\x0a" );
 	string target("\x0a@2021/04/16 16:25:29.388-Antenna1-U3000E28011606000020D6841ECBF4ABF\x0d\x0a");
-	const regex reg( "\n(@?)(\\d{4}/\\d{2}/\\d{2} \\d{2}:\\d{2}:\\d{2}\\.\\d{3})-Antenna(\\d+)-U([0-9a-fA-F]{4})(.*)([0-9a-fA-F]{4})\r\n" );
+	const regex reg( "\n(@?)(\\d{4}/\\d{2}/\\d{2} \\d{2}:\\d{2}:\\d{2}\\.\\d{3})-Antenna(\\d+)-U(.*)\r\n" );
 	smatch index_match;
 		if (std::regex_match(target, index_match, reg)) {
 			// The first sub_match is the whole string; the next
@@ -46,5 +46,18 @@ void regex_test2()
 				cout << "index_match[" << i << "]= " << m << endl;
 			}
 		}
+		string epc = index_match[4].str();
+		if (epc.size()) {
+			const regex epc_reg( "([0-9a-fA-F]{4})([0-9a-fA-F]*)([0-9a-fA-F]{4})" );
+			smatch epc_match;
+			if (std::regex_match(epc, epc_match, epc_reg)) {
+				for (int j = 0; j < epc_match.size(); j++) {
+					std::ssub_match sub_match = epc_match[j];
+					std::string m = sub_match.str();
+					cout << "epc_match[" << j << "]= " << m << endl;
+				}
+			}
+		}
+
 
 }
